c4d86c2932db52eaa5df6c4ff42b20fee00e3c08
JurassicPork <jurassic.pork@orange.fr>
2017-01-21 08:34:42 +0100

    lazsynaser replace synaser

--- test/main.pas
+++ test/main.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, LResources, Forms, Controls, Graphics, Dialogs, LazSerial,
-  StdCtrls, ExtCtrls, ComCtrls,inifiles,Math, synaser;
+  StdCtrls, ExtCtrls, ComCtrls,inifiles,Math, lazsynaser;
 
 type
 
